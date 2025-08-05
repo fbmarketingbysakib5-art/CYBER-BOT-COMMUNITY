46721eb41f7a3bc24a310b4957f48b4d2d49379c
fbmarketingbysakib9-jpg <fbmarketingbysakib9@gmail.com>
2025-08-05 18:57:32 +0600

    Update spy.js

--- Script/commands/spy.js
+++ Script/commands/spy.js
@@ -1,62 +1,57 @@
-module.exports = {
-  config: {
-    name: "spy",
-    version: "1.3",
-    hasPermssion: 0,
-    usePrefix: true,
-    credits: "YourName",
-    description: "Get basic user information",
-    commandCategory: "information",
-    cooldowns: 10,
-  },
+module.exports.config = {
+  name: "spy",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "CYBER",
+  description: "See someone's profile info",
+  commandCategory: "User",
+  usages: "[mention/reply/uid]",
+  cooldowns: 5,
+};
 
-  run: async function({ event, api, args, Users }) {
-    try {
-      let uid;
+module.exports.run = async function ({ api, event, args, Users }) {
+  const { threadID, messageID, type, messageReply, senderID, mentions } = event;
+  let uid;
 
-      // Get UID from args, mention, or reply
-      if (args[0]) {
-        if (/^\d+$/.test(args[0])) {
-          uid = args[0];
-        } else {
-          const match = args[0].match(/profile\.php\?id=(\d+)/);
-          if (match) uid = match[1];
-        }
-      }
-
-      if (!uid) {
-        uid = event.messageReply?.senderID || Object.keys(event.mentions)[0] || event.senderID;
-      }
-
-      const userName = await Users.getNameUser(uid);
-
-      let fbInfo;
-      try {
-        fbInfo = await api.getUserInfo(uid);
-        fbInfo = fbInfo[uid];
-      } catch {
-        fbInfo = null;
-      }
-
-      // ✅ Gender Mapping (same as first code)
-      let gender = "Unknown ❓";
-      if (fbInfo && typeof fbInfo.gender !== "undefined") {
-        if (fbInfo.gender === 1) gender = "Female 👩";
-        else if (fbInfo.gender === 2) gender = "Male 👨";
-      }
-
-      const name = fbInfo?.name || userName || "Unknown";
-
-      // Final Output
-      let message = `🔍 Basic User Information\n\n`;
-      message += `👤 Name: ${name}\n`;
-      message += `🆔 User ID: ${uid}\n`;
-      message += `🧬 Gender: ${gender}`;
-
-      return api.sendMessage(message, event.threadID, event.messageID);
-    } catch (error) {
-      console.log("Spy command error: ", error);
-      return api.sendMessage("❌ Error fetching user info.", event.threadID, event.messageID);
-    }
+  // Get UID from mention, reply, argument, or self
+  if (mentions && Object.keys(mentions).length > 0) {
+    uid = Object.keys(mentions)[0];
+  } else if (type === "message_reply") {
+    uid = messageReply.senderID;
+  } else if (args[0]) {
+    uid = args[0];
+  } else {
+    uid = senderID;
   }
+
+  // Try to get user info from Facebook API
+  let fbInfo;
+  try {
+    fbInfo = await api.getUserInfo(uid);
+    fbInfo = fbInfo[uid];
+  } catch (e) {
+    fbInfo = null;
+  }
+
+  // Try to get user info from local database
+  let userInfo = await Users.getData(uid);
+  userInfo = userInfo?.data || {};
+
+  // Gender
+  let gender = "Unknown ❓";
+  if (fbInfo && fbInfo.gender != null) {
+    gender = fbInfo.gender === 1 ? "Female 👩" : fbInfo.gender === 2 ? "Male 👨" : "Unknown ❓";
+  }
+
+  // Build message
+  let msg = `🔍 Profile Information 🔍\n\n`;
+  msg += `👤 Name: ${fbInfo?.name || "Not Found"}\n`;
+  msg += `🆔 UID: ${uid}\n`;
+  msg += `🧬 Gender: ${gender}\n`;
+  msg += `🎂 Birthday: ${userInfo.birthday || "Not set"}\n`;
+  msg += `📍 Address: ${userInfo.address || "Not set"}\n`;
+  msg += `🐣 Nickname: ${userInfo.nickname || "Not set"}\n`;
+  msg += `🌐 Profile: https://facebook.com/${uid}\n`;
+
+  return api.sendMessage(msg, threadID, messageID);
 };
